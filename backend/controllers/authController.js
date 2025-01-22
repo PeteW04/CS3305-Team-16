7d18c97d7afb64310f68c68c0e6df0b7043ce603
Pete White <petemurtwhite@gmail.com>
2025-01-22 21:51:57 +0000

    User login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 import User from "../models/User";
 import Organization from "../models/Organization";
-import { hashPassword } from "../utils/passwordHash";
+import { hashPassword, checkPassword } from "../utils/passwordHash";
 
 export const registerManager = async (req, res) => {
     const { firstName, lastName, email, password, organizationName, description } = req.body;
@@ -22,10 +22,33 @@ export const registerManager = async (req, res) => {
 
         await organization.save();
 
-        const token = jwt.sign({ id: manager._id }, process.env.JWT_SECRET, { expiresIn: "7D" })
+        const token = jwt.sign({ id: manager._id, role: manager.role }, process.env.JWT_SECRET, { expiresIn: "7d" })
         res.status(201).json({ token })
     } catch (e) {
         console.error("Error in registerManager:", e.message);
         res.status(500).json({ message: e.message });
     }
-}
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).send({ message: "User does not exist" });
+        }
+
+        const correctPassword = await checkPassword(password, user.password);
+        if (!correctPassword) {
+            return res.status(400).send({ message: "Invalid Password" })
+        }
+
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+        res.status(200).json({ token });
+    } catch (e) {
+        console.error("Login Error", e.message);
+        res.status(500).json({ message: e.message });
+    }
+};
--- backend/utils/passwordHash.js
+++ backend/utils/passwordHash.js
@@ -6,9 +6,10 @@ export const hashPassword = async (password) => {
     try {
         const salt = await bcrypt.genSalt(SALT_ROUNDS);
         const hashedPassword = await bcrypt.hash(password, salt);
-        return hashPassword;
+        return hashedPassword;
     }
     catch (e) {
+        console.error("Hash Password Error", e.message)
         throw new Error('Error hashing password');
     }
 }
@@ -16,7 +17,8 @@ export const hashPassword = async (password) => {
 export const checkPassword = async (password, hashedPassword) => {
     try {
         isCorrect = await bcrypt.compare(password, hashedPassword);
-    } catch (err) {
+    } catch (e) {
+        console.error("Check Password Error", e.message)
         throw new Error('Error verifying password');
     }
 }
