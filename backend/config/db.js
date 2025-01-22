903a9a9435d18f848440add757a72031457f7393
Pete White <petemurtwhite@gmail.com>
2025-01-22 16:20:04 +0000

    Initial database connection setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,17 @@
-const placeholder = null;
+import mongodb from 'mongodb';
+
+const MongoClient = mongodb.MongoClient;
+const uri = "mongodb+srv://pete:CS3305-Team16%23%23@team-16.eny86.mongodb.net/?retryWrites=true&w=majority&appName=Team-16";
+const client = new MongoClient(uri);
+
+const connectDB = async () => {
+    try {
+        await client.connect(); // Connect to the database
+        console.log('MongoDB connected...');
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err.message);
+        process.exit(1); // Exit with failure
+    }
+};
+
+export default connectDB;
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,1 @@
-const placeholder = null;
+
