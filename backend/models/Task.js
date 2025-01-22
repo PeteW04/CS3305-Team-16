8fe6c9dbb7925cf15f82fb369c41ae0ca5a80fe3
Mark Burns <maekburns@gmail.com>
2025-01-22 20:17:54 +0000

    Added taskSchema. i have a todo with some more ideas that we can look in to in the future. basic functionality added here

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,1 +1,24 @@
-const placeholder = null;
+import mongoose from 'mongoose';
+
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String, required: true
+  },
+  description: {
+    type: String, default: ''
+  },
+  status: {
+    type: String, enum: ['New', 'In Progress', 'Completed'], default: 'New'
+  },
+  deadline: {
+    type: Date
+  }, 
+});
+// TODO
+// Add a new field about assignedTo
+// Add a timestamp??
+// Add a assignedBy?? to track which manager assigned the task??
+
+
+export default mongoose.model('Task', taskSchema);
