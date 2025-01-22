1293e424d168bc1e7042ba5509455ab6c225ff81
Mark Burns <maekburns@gmail.com>
2025-01-22 19:23:50 +0000

    Added 'Read all tasks' route

--- backend/routers/taskRouter.js
+++ backend/routers/taskRouter.js
@@ -1,1 +1,14 @@
-const placeholder = null;
+const express = require('express');
+const router = express.Router();
+
+// Read all tasks
+router.get('/', (req, res, next) => {
+    try{
+        res.send('Get all tasks');
+    }
+    catch (error) {
+        res.status(500).send();
+    }
+});
+
+
