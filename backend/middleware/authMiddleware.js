edbe678cd7a9ddce4ac98e7e9f6692778b4f2efe
Pete White <petemurtwhite@gmail.com>
2025-01-26 12:40:17 +0000

    Authentication middleware added

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,1 +1,19 @@
-const placeholder = null;
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+export const authenticate = async (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(400).json({ message: "Unauthorized" });
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decodedToken.id).select('-password');
+        next();
+    } catch (e) {
+        console.error("Authentication Error: ", e.message);
+        res.status(400).json({ message: 'Token invalid/expired' });
+    }
+}
--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -1,1 +1,17 @@
-const placeholder = null;
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
