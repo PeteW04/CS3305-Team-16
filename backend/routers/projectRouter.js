6d261dd0a62f5439b6af3623b7b2c4c32846cfce
rodonan <ronanod2003@gmail.com>
2025-01-29 12:36:54 +0000

    updated router names

--- backend/routers/projectRouter.js
+++ backend/routers/projectRouter.js
@@ -1,27 +1,27 @@
 const express = require('express');
 import { createProject, getAllProjects, getProjectById, addEmployee, addTask, updateTaskStatus, updateStatus } from '../controllers/projectController';
 
-const router = express.Router();
+const projectRouter = express.Router();
 
 // Get all projects
-router.get('/', getAllProjects);
+projectRouter.get('/', getAllProjects);
 
 // Get project by id
-router.get('/:id', getProjectById);
+projectRouter.get('/:id', getProjectById);
 
 // Create new project
-router.post('/', createProject);
+projectRouter.post('/', createProject);
 
 // Add an employee
-router.put('/:projectId/employees/:employeeId', addEmployee);
+projectRouter.put('/:projectId/employees/:employeeId', addEmployee);
 
 // Add a task
-router.put('/:projectId/tasks/:taskId', addTask);
+projectRouter.put('/:projectId/tasks/:taskId', addTask);
 
 // Update a task's status ['New', 'In Progress', 'Completed']
-router.put('/:projectId/tasks/:taskId/:status', updateTaskStatus);
+projectRouter.put('/:projectId/tasks/:taskId/:status', updateTaskStatus);
 
 // Update the projects status ['New', 'In Progress', 'Completed']
-router.put('/:projectId/:status', updateStatus);
+projectRouter.put('/:projectId/:status', updateStatus);
 
-module.export = router;
+module.export = projectRouter;
