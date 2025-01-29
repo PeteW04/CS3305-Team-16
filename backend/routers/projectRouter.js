cec6c42689c31ad3d1d601c2240cfe452065e47b
rodonan <ronanod2003@gmail.com>
2025-01-29 22:34:57 +0000

    added functions to project controller

--- backend/routers/projectRouter.js
+++ backend/routers/projectRouter.js
@@ -7,17 +7,35 @@ const projectRouter = express.Router();
 // Get all projects
 projectRouter.get('/', getAllProjects);
 
+// GET: List all Projects in an Organization
+projectRouter.get('/:org', getAllProjects);
+
 // Get project by id
 projectRouter.get('/:id', getProjectById);
 
 // Create new project
 projectRouter.post('/', checkUserRole('manager'), createProject);
 
+// Change project name
+projectRouter.put('/:title',  checkUserRole('manager'), addEmployee);
+
+// Change project description
+projectRouter.put('/:description',  checkUserRole('manager'), addEmployee);
+
+// Change project deadline
+projectRouter.put('/:deadline',  checkUserRole('manager'), addEmployee);
+
 // Add an employee
-projectRouter.put('/:projectId/employees/:employeeId',  checkUserRole('manager'), addEmployee);
+projectRouter.put('/:projectId/employees/add/:employeeId',  checkUserRole('manager'), addEmployee);
+
+// Remove an Employee
+projectRouter.put('/:projectId/employees/remove/:employeeId',  checkUserRole('manager'), addEmployee);
 
 // Add a task
-projectRouter.put('/:projectId/tasks/:taskId',  checkUserRole('manager'), addTask);
+projectRouter.put('/:projectId/tasks/add/:taskId',  checkUserRole('manager'), addTask);
+
+// Remove a task
+projectRouter.put('/:projectId/tasks/remove/:taskId',  checkUserRole('manager'), addTask);
 
 // Update a task's status ['New', 'In Progress', 'Completed']
 projectRouter.put('/:projectId/tasks/:taskId/:status', updateTaskStatus);
