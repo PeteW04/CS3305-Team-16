4b60bfb03dc45cb173f0acec84eb48f8c17756a9
Feilim White <feilimwhite@gmail.com>
2025-01-30 17:38:02 +0000

    Revised files structure
    Neater file structure
    -Everything still works files are just neater now

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './Login';
-import Signup from './Signup';
-import Landing from './Landing';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
