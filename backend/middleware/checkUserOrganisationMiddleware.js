d8e1005a133537876bde2ed6ccb75353186a5ae7
Mark Burns <maekburns@gmail.com>
2025-01-30 01:43:50 +0000

    Added the organisation ID to the manager and employee JWT
    Changed checkUserOrganisationMiddleware to verify the JWT

--- backend/middleware/checkUserOrganisationMiddleware.js
+++ backend/middleware/checkUserOrganisationMiddleware.js
@@ -1,10 +1,26 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
-export const checkUserOrganisation = (requiredOrganisation) => async (req, res, next) => {
-    if (!req.user || req.user.organisation !== requiredOrganisation) {
-        return res.status(400).json({ message: "Unauthorized" });
+export const checkUserOrganisation = async (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ error: 'Authorization token missing' });
     }
 
-    next();
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decodedToken; // allows me to access all of the user info cause idk what I actually need
+
+        const orgId = req.params.organizationId || req.body.organizationId; // params for if the id is in the URL, body if its in the request itself -- idk whats right but this is safe
+
+        // check if the organisationID is correct
+        if (decodedToken.organizationId !== orgId) {
+            return res.status(400).json({ message: "Unauthorized" });
+        }
+
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: 'Invalid token' });
+    }
 };
