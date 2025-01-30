95ea11085b82f6da6c0f4540901e33fa489ce4cb
Mark Burns <maekburns@gmail.com>
2025-01-30 14:37:09 +0000

    included organisation checks to taskController to all CRUD methods Added project ID to task model so we are less vulnerable to URL API execution

--- backend/middleware/checkUserOrganisationMiddleware.js
+++ backend/middleware/checkUserOrganisationMiddleware.js
@@ -12,13 +12,6 @@ export const checkUserOrganisation = async (req, res, next) => {
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decodedToken; // allows me to access all of the user info cause idk what I actually need
 
-        const orgId = req.params.organizationId || req.body.organizationId; // params for if the id is in the URL, body if its in the request itself -- idk whats right but this is safe
-
-        // check if the organisationID is correct
-        if (decodedToken.organizationId !== orgId) {
-            return res.status(400).json({ message: "Unauthorized" });
-        }
-
         next();
     } catch (error) {
         return res.status(401).json({ error: 'Invalid token' });
