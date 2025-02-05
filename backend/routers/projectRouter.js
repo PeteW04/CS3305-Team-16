697da75c9dc6fe288a68b48ca69b4fcd51acfdc3
rodonan <162589203+rodonan@users.noreply.github.com>
2025-02-05 21:36:12 +0000

    Testing and bug fixes in projectController and projectRouter

--- backend/routers/projectRouter.js
+++ backend/routers/projectRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createProject, getAllProjects, getProjectById, addEmployee, addTask, updateTaskStatus, updateStatus } from '../controllers/projectController.js';
+import { createProject, getAllProjects, getAllProjectsByOrg, getProjectById, changeTitle, changeDescription, changeDeadline, addEmployee, removeEmployee, addTask, removeTask, updateTaskStatus, updateStatus } from '../controllers/projectController.js';
 import { checkUserRole } from '../middleware/checkUserRoleMiddleware.js';
 
 const projectRouter = express.Router();
@@ -8,39 +8,39 @@ const projectRouter = express.Router();
 projectRouter.get('/', getAllProjects);
 
 // GET: List all Projects in an Organization
-projectRouter.get('/:org', getAllProjects);
+projectRouter.get('/org/:org', getAllProjectsByOrg);
 
 // Get project by id
-projectRouter.get('/:id', getProjectById);
+projectRouter.get('/projectId/:projectId', getProjectById);
 
 // Create new project
 projectRouter.post('/', checkUserRole('manager'), createProject);
 
 // Change project name
-projectRouter.put('/:title', checkUserRole('manager'), addEmployee);
+projectRouter.put('/change-title/projectId/:projectId/newTitle/:newTitle', checkUserRole('manager'), changeTitle);
 
 // Change project description
-projectRouter.put('/:description', checkUserRole('manager'), addEmployee);
+projectRouter.put('/change-description/projectId/:projectId/newDescription/:newDescription', checkUserRole('manager'), changeDescription);
 
 // Change project deadline
-projectRouter.put('/:deadline', checkUserRole('manager'), addEmployee);
+projectRouter.put('/change-deadline/projectId/:projectId/newDeadline/:newDeadline', checkUserRole('manager'), changeDeadline);
 
 // Add an employee
-projectRouter.put('/:projectId/employees/add/:employeeId', checkUserRole('manager'), addEmployee);
+projectRouter.put('/projectId/:projectId/employees/add/employeeId/:employeeId', checkUserRole('manager'), addEmployee);
 
 // Remove an Employee
-projectRouter.put('/:projectId/employees/remove/:employeeId', checkUserRole('manager'), addEmployee);
+projectRouter.put('/projectId/:projectId/employees/remove/employeeId/:employeeId', checkUserRole('manager'), removeEmployee);
 
 // Add a task
-projectRouter.put('/:projectId/tasks/add/:taskId', checkUserRole('manager'), addTask);
+projectRouter.post('/projectId/:projectId/tasks/add', checkUserRole('manager'), addTask);
 
 // Remove a task
-projectRouter.put('/:projectId/tasks/remove/:taskId', checkUserRole('manager'), addTask);
+projectRouter.put('/projectId/:projectId/tasks/remove/taskId/:taskId', checkUserRole('manager'), removeTask);
 
 // Update a task's status ['New', 'In Progress', 'Completed']
-projectRouter.put('/:projectId/tasks/:taskId/:status', updateTaskStatus);
+projectRouter.put('/projectId/:projectId/tasks/taskId/:taskId/status/:status', updateTaskStatus);
 
 // Update the projects status ['New', 'In Progress', 'Completed']
-projectRouter.put('/:projectId/:status', updateStatus);
+projectRouter.put('/projectId/:projectId/status/:status', updateStatus);
 
 export default projectRouter;
