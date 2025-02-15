cd7169c1bdee5c8d73666f28dce2bd95dc14a153
Feilim White <feilimwhite@gmail.com>
2025-02-15 18:43:36 +0000

    created a chat message component
    chat message component which includes is sent and time variables

--- frontend/src/pages/Component-Test.js
+++ frontend/src/pages/Component-Test.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Sidebar from "../components/sidebar";
+import ChatBubble from "../components/ChatMessage";
 
 function TestPage() {
   const [isMinimized, setIsMinimized] = useState(false);
@@ -16,7 +17,9 @@ function TestPage() {
       {/* Main Content */}
       <div className="flex-1 bg-gray-100 py-10 px-6">
         <h1 className="text-xl font-bold">Test Page for Components</h1>
+        <ChatBubble sender={"Feilim"} message={"Hello blah blah blah blah"} time={Date} isSent={true}/>
       </div>
+      
     </main>
   );
 }
