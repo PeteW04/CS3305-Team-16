aadad716fc10a0ac9ad11320e8311db263d42b0b
Matthew Glavin <matthewglavin2004@gmail.com>
2025-02-20 13:14:40 +0000

    removed tasks link from header - as user now must be logged in to access tasks (protected route)

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -29,12 +29,6 @@ function Header() {
               >
                 Sign Up Free
               </Link>
-              <Link
-                to="/tasks"
-                className="bg-indigo-600 text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-indigo-700 transition-colors"
-              >
-                Tasks
-              </Link>
             </div>
           )}
         </div>
