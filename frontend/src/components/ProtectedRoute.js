c7c0fcee54d374c2f96350c619b29c244b909afc
Pete White <petemurtwhite@gmail.com>
2025-02-24 15:05:32 +0000

    Spiner import update in protected route

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import Spinner from "./Spinner";
+import { Spinner } from "./Spinner";
 
 const ProtectedRoute = () => {
     const { user, isLoading } = useAuth();
