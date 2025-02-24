c3d6fc129515a1404166cb17d1f1431e646c1c8e
eamun <122382926@umail.ucc.ie>
2025-02-24 23:03:11 +0000

    added the dashboard components to the test page.

--- frontend/src/pages/Component-Test.js
+++ frontend/src/pages/Component-Test.js
@@ -3,6 +3,8 @@ import Sidebar from "../components/sidebar";
 import ChatBubble from "../components/chatBubble";
 import TaskManager from "../components/TaskManager";
 import ProjectSummary from "../components/ProjectSummary";
+import ProgressGauge from "../components/ProgressGauge";
+import Calendar from "../components/Calendar";
 
 function TestPage() {
   const [isMinimized, setIsMinimized] = useState(false);
@@ -15,17 +17,29 @@ function TestPage() {
     <main className="flex h-screen pt-16">
       {/* Sidebar */}
       <Sidebar isMinimized={isMinimized} toggleSidebar={toggleSidebar} />
-      {/* Project summary */}
-      <ProjectSummary />
-      {/* tasklist */}
-      <TaskManager />
 
-      {/* Main Content */}
-      <div className="flex-1 bg-gray-100 py-10 px-6">
-        <h1 className="text-xl font-bold">Test Page for Components</h1>
-        <ChatBubble sender={"Feilim"} message={"Hello blah blah blah blah"} time={Date} isSent={true} />
+      <div className="dashboard" style={{flexDirection: "row",display:"flex", flexWrap: "wrap"}}>
+        
+        {/* tasklist */}
+        <div><TaskManager /></div>
+        {/* Progress Gauge */}
+        <div><ProgressGauge /></div>
+        {/* Calendar */}
+        <div><Calendar /></div>
+        {/* Project summary */}
+        <div><ProjectSummary /></div>
       </div>
 
+      {/* Main Content 
+      <div className="flex-1 bg-gray-100 py-10 px-6">
+        <h1 className="text-xl font-bold">Test Page for Components</h1>
+        <ChatBubble
+          sender={"Feilim"}
+          message={"Hello blah blah blah blah"}
+          time={Date}
+          isSent={true}
+        />
+      </div>*/}
     </main>
   );
 }
