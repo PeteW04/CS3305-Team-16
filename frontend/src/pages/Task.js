b5d26a301a12e61c6b775dbe5f26ba4bf3d9f2dc
rodonan <162589203+rodonan@users.noreply.github.com>
2025-02-25 15:58:11 +0000

    Taskboard now says project name at the top instead of project id

--- frontend/src/pages/Task.js
+++ frontend/src/pages/Task.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useParams } from "react-router-dom";
 import { Menu, Bell, Search, User } from "lucide-react";
 import Taskboard from "../components/Taskboard";
 import Sidebar from "../components/sidebar";
 import { useState } from "react";
 import ProfileModal from '../components/ProfileModal';
+import { getProject } from '../api/project.js'
 
 function Task() {
     const [isMinimized, setIsMinimized] = useState(false);
     const [showProfileModal, setShowProfileModal] = useState(false);
     const { projectId } = useParams();
+    const [projectName, setProjectName] = useState(null);
+
+    useEffect(() => {
+      const fetchProject = async () => {
+          try {
+              const project = await getProject(projectId);
+              setProjectName(project.title || "Project Not Found");  // Assuming `data.name` contains project name
+          } catch (error) {
+              console.error("Error fetching project:", error);
+              setProjectName("Error Loading Project");
+          }
+      };
+
+      fetchProject();
+    }, [projectId]);
 
     const toggleSidebar = () => {
       setIsMinimized(!isMinimized);
     };
   
-
   return (
     <div className="min-h-screen bg-gray-50 h-screen flex flex-col">
       {/* Header */}
@@ -57,7 +72,7 @@ function Task() {
         <main className="p-6 flex-1 h-full overflow-y-auto">
           <div className="mb-6">
             <h1 className="text-2xl font-bold text-gray-900">
-              {projectId}
+              {projectName}
             </h1>
             <h2 className="text-2xl font-bold text-gray-900">
               Task Management
