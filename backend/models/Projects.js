6785b379bb6e3563ec7e85d58dc152fdaed8087c
rodonan <ronanod2003@gmail.com>
2025-02-26 12:29:53 +0000

    added manager attribute to projects schema

--- backend/models/Projects.js
+++ backend/models/Projects.js
@@ -6,6 +6,7 @@ const projectSchema = new mongoose.Schema({
   organization: {type: mongoose.Schema.Types.ObjectId, ref: 'Organization', required: true},
   tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task', default: [] }],
   chat: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat'},
+  manager: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   employees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: []}],
   status: {type: String, enum: ['New', 'In Progress', 'Completed'], default: 'New'},
   completion: {type: Number, min: 0, max: 100, default: 0}, 
