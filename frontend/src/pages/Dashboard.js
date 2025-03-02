7782d7f6ea718723ad0ace1fa81932b0c7636f3a
eamun <122382926@umail.ucc.ie>
2025-03-02 15:12:55 +0000

    Updated dashboard layout and fixed calendar project badge positioning

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -9,14 +9,14 @@ import "../CSS-files/Dashboard.css";
 function Dashboard() {
   return (
     <div className="p-6">
-      <div className="dashboard" style={{display:"inline-flex"}}>
+      <div className="dashboard" style={{display:"flex", flexDirection: "row"}}>
         <div className="dashboardother" style={{flexDirection: "row", display:"flex", flexWrap: "wrap"}}>
           <div><ProjectSummary /></div>
           <div><Calendar /></div>
           <div><TaskManager /></div>
           <div><ProgressGauge /></div>
         </div>
-        <div className="chat">
+        <div className="chat" style={{position:"sticky", right:"0", top:"0", width:"fit-content", height:"100vh"}}>
           <div><MessageList /></div>
         </div>
       </div>
